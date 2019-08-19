ea44ea89e749138b609e1d5412d2075cd0c242b1
cottonatocd <cotton@ocdagency.com>
2019-08-19 17:25:48 -0400

    more mods

--- js/script.js
+++ js/script.js
@@ -25,9 +25,8 @@ document.getElementById("inputMono").innerHTML = '';
 for (var i=0; i<lettersSans.length; i++){
     document.getElementById("inputSans").innerHTML += "<span class='letterSans' id='letter" + i +"'>" + lettersSans[i] + "</span>";
     let printVal = document.getElementById("letter" + i).offsetWidth;
-    console.log(document.getElementById("letter" + i).offsetWidth, document.getElementById("letter" + i).clientWidth)
     document.getElementById("inputSansValues").innerHTML += '<span class="d-inline-block" style="width: ' + printVal + 'px">' + document.getElementById("letter" + i).offsetWidth + '</span>';
-    let tint = map(document.getElementById("letter" + i).offsetWidth, lettersMinWidth, lettersMaxWidth, 80,50);
+    let tint = interpolate(document.getElementById("letter" + i).offsetWidth, lettersMinWidth, lettersMaxWidth, 80,50);
     document.getElementById("letter" + i).style.backgroundColor = "hsl(221,100%," + tint + "%)";
 } 
 
@@ -61,18 +60,68 @@ window.onload = function start() {
 function switchLetters() {
     window.setInterval(function () {
         separateLettersMono(monoWords[c]);
-        if (c<monoWords.length){c++} else {c=0}
+        if (c<monoWords.length-1){c++} else {c=0;};
+        // console.log(c);
+        // console.log(monoWords.length);
     }, 3000); // repeat forever, polling every 3 seconds
 }
 
 document.getElementById("board").onmousemove = function(e){
-    document.getElementById("boardCursor").style.position = "fixed";
     document.getElementById("avocado").classList.remove("active");
-    // let mouseX = map(e.clientX, 0, window.innerWidth, 0, document.getElementById("board").clientWidth);
-    // let mouseY = map(e.clientY, 0, window.innerHeight, 0, document.getElementById("board").clientHeight);
+    let bounds = document.getElementById("board").getBoundingClientRect();
+    let mouseX = interpolate(e.clientX, bounds.left, bounds.right, 0, document.getElementById("board").clientWidth);
+    let mouseY = interpolate(e.clientY, bounds.top, bounds.bottom, 0, document.getElementById("board").clientHeight);
+    document.getElementById("boardCursor").style.top = mouseY + 5 + "px";
+    document.getElementById("boardCursor").style.left = mouseX + 5 + "px";
+}
 
-    document.getElementById("boardCursor").style.top = e.clientY + 5 + "px";
-    document.getElementById("boardCursor").style.left = e.clientX + 5 + "px";
+document.getElementById("canvas").onmousemove = function(e){
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    // let bounds = document.getElementById("dollars").getBoundingClientRect();
+    // let mouseX = interpolate(e.clientX, bounds.left, bounds.right, 0, document.getElementById("dollars").clientWidth);
+    // let mouseY = interpolate(e.clientY, bounds.top, bounds.bottom, 0, document.getElementById("dollars").clientHeight);
+    // console.log(bounds, mouseX, mouseY);
+    // console.log(Math.round(20*mouseX)/20);
+    // let xPos = Math.ceil(mouseX/60)*60 - 400;
+    // let yPos = Math.ceil(mouseY/110)*110 - 450;
+    let xPos = Math.ceil(mouseX/60)*60 - 60;
+    // let yPos = Math.ceil(mouseY/110)*110 - 110;
+    let yPos = mouseY;
+    let fontVarSettings = 'font-variation-settings: "PROP" 500, "XPRN" 200, "ital" 500, "CRSV" 456, "wght" ' + 300 +'';
+    document.getElementById("dollars").innerHTML += "<div class='dollar' style='top: " + yPos  + "px; left:" + xPos  + "px'>$</div>";
+
+    
+    let bounds = document.getElementById("dollars").getBoundingClientRect();
+    mouseX = interpolate(e.clientX, bounds.left, bounds.right, 0, document.getElementById("dollars").clientWidth);
+    mouseY = interpolate(e.clientY, bounds.top, bounds.bottom, 0, document.getElementById("dollars").clientHeight);
+    document.getElementById("yAxis").style.left = mouseX + "px";
+    document.getElementById("xAxis").style.top = mouseY + "px";
+}
+
+
+// document.getElementById("canvas").onmousemove = function(e){
+//     if (enableHandler) {
+//         handleMouseMove(e);
+//         enableHandler = false;
+//     }
+
+//     let bounds = document.getElementById("dollars").getBoundingClientRect();
+//     let mouseX = interpolate(e.clientX, bounds.left, bounds.right, 0, document.getElementById("dollars").clientWidth);
+//     let mouseY = interpolate(e.clientY, bounds.top, bounds.bottom, 0, document.getElementById("dollars").clientHeight);
+//     document.getElementById("yAxis").style.left = mouseX + "px";
+//     document.getElementById("xAxis").style.top = mouseY + "px";
+// }
+
+timer = window.setInterval(function(){
+    enableHandler = true;
+}, 100);
+
+function handleMouseMove(e){
+    let bounds = document.getElementById("dollars").getBoundingClientRect();
+    let mouseX = interpolate(e.clientX, bounds.left, bounds.right, 0, document.getElementById("dollars").clientWidth);
+    let mouseY = interpolate(e.clientY, bounds.top, bounds.bottom, 0, document.getElementById("dollars").clientHeight);
+    document.getElementById("dollars").innerHTML += "<div class='dollar' style='top: " + mouseY + "px; left:" + mouseX + "px'>$</div>";
 }
 
 
@@ -90,6 +139,6 @@ function separateLettersMono(string){
     } 
 }
 
-function map(value, low1, high1, low2, high2) {
+function interpolate(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
